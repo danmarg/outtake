a7baa107c7a5ea9ce3816382005b0d2eecbb445b
Daniel Margolis <dan@af0.net>
2020-06-20 15:29:23 +0200

    Upgrade and switch to modules.

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/danmarg/outtake
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/urfave/cli/v2 v2.2.0
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.28.0
+)
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/danmarg/outtake/lib"
 	"github.com/danmarg/outtake/lib/gmail"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 	"os"
 	"time"
 )
@@ -14,59 +14,56 @@ const (
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "outtake"
-	app.Usage = "Export Gmail to Maildir...efficiently!"
-	app.Version = "0.0.1"
-	app.Author = "dan@af0.net"
+	app := &cli.App{
+		Name:    "outtake",
+		Usage:   "Export Gmail to Maildir...efficiently!",
+		Version: "0.0.1",
+		Authors: []*cli.Author{&cli.Author{
+			Name: "Daniel Margolis", Email: "dan@af0.net"}},
+	}
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "directory",
 			Usage: "Maildir to output to.",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "full",
 			Usage: "Force a full sync",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "label",
 			Usage: "Label to sync",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "buffer",
 			Usage: "Download buffer size",
 			Value: 128,
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "parallel",
 			Usage: "Max parallel downloads",
 			Value: 8,
 		},
 	}
-	app.Action = func(ctx *cli.Context) {
+	app.Action = func(ctx *cli.Context) error {
 		d := ctx.String("directory")
 		if d == "" {
-			fmt.Println("Missing --directory flag")
-			return
+			return fmt.Errorf("Missing --directory flag")
 		}
 		if s, err := os.Stat(d); err != nil && os.IsNotExist(err) {
 			if err := os.MkdirAll(d, 0766); err != nil {
-				fmt.Println("Error: ", err)
-				return
+				return err
 			}
 		} else if err != nil {
-			fmt.Println("Error: ", err)
-			return
+			return err
 		} else if !s.IsDir() {
-			fmt.Printf("Error: %d exists and is not a directory\n", d)
-			return
+			return fmt.Errorf("Error: %d exists and is not a directory\n", d)
 		}
 		g, err := gmail.NewGmail(d, ctx.String("label"))
 		gmail.MessageBufferSize = ctx.Int("buffer")
 		gmail.ConcurrentDownloads = ctx.Int("parallel")
 		if err != nil {
-			fmt.Println("Error: ", err)
-			return
+			return err
 		}
 		progress := make(chan lib.Progress)
 		go func() {
@@ -83,7 +80,7 @@ func main() {
 			fmt.Println(err)
 			os.Exit(-1)
 		}
-
+		return nil
 	}
 	app.Run(os.Args)
 }
