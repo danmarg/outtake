7cfe557c00840a95627ee5f078d3c365a4368a82
Taras Glek <taras@glek.net>
2026-02-28 21:10:51 +0200

    fix(v2): avoid sqlite busy locks with single-conn mode and busy timeout

--- lib/gmail/message_sync.go
+++ lib/gmail/message_sync.go
@@ -37,6 +37,10 @@ func (g *Gmail) SyncListedMessages(dbPath string) error {
 		return err
 	}
 	defer db.Close()
+	db.SetMaxOpenConns(1)
+	if _, err := db.Exec(`PRAGMA busy_timeout = 5000`); err != nil {
+		return err
+	}
 
 	if err := ensureListPagesSchema(db); err != nil {
 		return err
